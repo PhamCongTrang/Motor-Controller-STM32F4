01dab5ac835c1c72e0fd09f10feef08dde127b73
unknown <trang.pc200631@sis.hust.edu.vn>
2023-02-06 22:53:04 +0700

    Creat project

---
+++ MDK-ARM/RTE/_Motor_Controller_STM32F4/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Motor Controller STM32F4' 
+ * Target:  'Motor Controller STM32F4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
